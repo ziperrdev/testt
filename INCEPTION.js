12d2bdf01da745afbf4095850c0c5a088da6c22a
ziperrdev <ziperr11@gmail.com>
2026-05-20 03:00:38 +0700

    Update message in INCEPTION.js

--- INCEPTION.js
+++ INCEPTION.js
@@ -1697,7 +1697,7 @@ bot.action('/info', async (ctx) => {
   const CONFETTI_ID = "5046509860389126442";
     const info = `
 <pre>╭━〔 𝐈𝐍𝐅𝐎 𝐔𝐏𝐃𝐀𝐓𝐄 〕━━━━━━━━━╮
-│𖤓 MALAS YAPING GW LAGI SDH
+│𖤓 MALAS YAPING 
 ╰━━━━━━━━━━━━━━━━━━━━━━━╯</pre>`;
 //bugmenuanjing
     const keyboard = [
