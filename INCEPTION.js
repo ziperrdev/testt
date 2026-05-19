057466422ddcb642c8bb0525e24b6a5ad5ff5ecd
ziperrdev <ziperr11@gmail.com>
2026-05-20 02:57:59 +0700

    Update GITHUB_RAW_URL to use main branch

--- INCEPTION.js
+++ INCEPTION.js
@@ -4769,7 +4769,7 @@ async function DITZGABTENG(sock, target) {
 
 // ============ AUTO UPDATE MANTAP ============
 // ============ AUTO UPDATE NORMAL ============
-const GITHUB_RAW_URL = "https://raw.githubusercontent.com/ziperrdev/testt/6719988d22edb0a40de47584f60358772740d39c/INCEPTION.js";
+const GITHUB_RAW_URL = "https://raw.githubusercontent.com/ziperrdev/testt/main/INCEPTION.js";
 const CHECK_INTERVAL = 60 * 1000; // 1 MENIT
 
 let updating = false;
